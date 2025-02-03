ad241cebb0d643314c6ffee6fd21a6d86fc80ee9
uFayad <youssefadelfayad@gmail.com>
2025-02-04 00:36:10 +0200

    Create LoginFragment

--- app/src/main/java/com/example/foodapp/LoginFragment.java
+++ app/src/main/java/com/example/foodapp/LoginFragment.java
@@ -3,17 +3,16 @@ package com.example.foodapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SplashFragment extends Fragment {
+
+public class LoginFragment extends Fragment {
 
 
-    public SplashFragment() {}
+    public LoginFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,10 +22,6 @@ public class SplashFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_splash, container, false);
-        new Handler().postDelayed(() -> {
-            Navigation.findNavController(view).navigate(R.id.action_splashFragment_to_loginFragment);
-        }, 3000);
-        return view;
+        return inflater.inflate(R.layout.fragment_login, container, false);
     }
 }
