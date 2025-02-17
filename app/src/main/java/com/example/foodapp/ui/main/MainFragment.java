ccec162aab6d9a436004889e0d6b337c0eb4c9d5
uFayad <youssefadelfayad@gmail.com>
2025-02-17 02:31:56 +0200

    Enhance UI for Login, Register, and Main Fragments

--- app/src/main/java/com/example/foodapp/ui/main/MainFragment.java
+++ app/src/main/java/com/example/foodapp/ui/main/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.foodapp.ui.main;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
--- app/src/main/java/com/example/foodapp/ui/splash/view/SplashFragment.java
+++ app/src/main/java/com/example/foodapp/ui/splash/view/SplashFragment.java
@@ -1,5 +1,6 @@
 package com.example.foodapp.ui.splash.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -23,6 +24,17 @@ public class SplashFragment extends Fragment implements SplashContract.View {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requireActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
+        requireActivity().getWindow().setNavigationBarColor(Color.TRANSPARENT);
+
+        requireActivity().getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR|
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR |
+                        View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        );
         presenter = new SplashPresenter(this, requireContext());
     }
 
